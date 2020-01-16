575095eedaacaa83b50bf39745c3fcb2d3c91f1a
Grigory Rylov <grishberg@gmail.com>
2020-01-16 23:18:27 +0300

    find contours

--- main.go
+++ main.go
@@ -16,31 +16,72 @@ import (
 	"fmt"
 	"gocv.io/x/gocv"
 	"image"
+	"image/color"
 	"os"
 )
+
+const MinimumArea = 3000
 
 type CounterRecognizer struct {
 	readedImage gocv.Mat
 }
 
-func (c *CounterRecognizer) Process() {
-	gray := c.gray(&c.readedImage)
-	blured := c.blur(&gray)
+func (c *CounterRecognizer) Process() *gocv.Mat {
+	gray := c.gray(c.readedImage)
+	defer gray.Close()
+
+	blurred := c.blur(gray)
+	defer blurred.Close()
+
+	edged := c.canny(blurred)
+
+	c.findContours(edged)
+
+	return edged
 }
 
-func (c *CounterRecognizer) blur(src *gocv.Mat) gocv.Mat {
-	var result gocv.Mat
+func (c *CounterRecognizer) findContours(img *gocv.Mat) {
+	contours := gocv.FindContours(*img, gocv.RetrievalExternal, gocv.ChainApproxSimple)
+	dst := &c.readedImage
+	for i, c := range contours {
+		area := gocv.ContourArea(c)
+		if area < MinimumArea {
+			continue
+		}
+
+		statusColor := color.RGBA{255, 0, 0, 0}
+		gocv.DrawContours(dst, contours, i, statusColor, 2)
+
+		rect := gocv.BoundingRect(c)
+		gocv.Rectangle(dst, rect, color.RGBA{0, 0, 255, 0}, 2)
+	}
+
+}
+
+func (c *CounterRecognizer) blur(src *gocv.Mat) *gocv.Mat {
+	result := gocv.NewMat()
+
 	imagePoint := image.Point{5, 5}
 	var borderType gocv.BorderType
 	borderType = 0
 	gocv.GaussianBlur(c.readedImage, &result, imagePoint, 0, 0, borderType)
-	return result
+	return &result
 }
 
-func (c *CounterRecognizer) gray(image *gocv.Mat) gocv.Mat {
-	var result gocv.Mat
-	gocv.CvtColor(*image, &result, gocv.ColorRGBToGray)
-	return result
+func (c *CounterRecognizer) gray(image gocv.Mat) *gocv.Mat {
+	result := gocv.NewMat()
+	gocv.CvtColor(image, &result, gocv.ColorRGBToGray)
+	return &result
+}
+
+func (c *CounterRecognizer) canny(image *gocv.Mat) *gocv.Mat {
+	result := gocv.NewMat()
+	gocv.Canny(*image, &result, 50, 200)
+	return &result
+}
+
+func (c *CounterRecognizer) saveToFile(image *gocv.Mat) {
+	gocv.IMWrite("out.jpg", *image)
 }
 
 func main() {
@@ -56,6 +97,11 @@ func main() {
 		fmt.Printf("Error reading image from: %v\n", filename)
 		return
 	}
+
+	recognizer := CounterRecognizer{img}
+	res := recognizer.Process()
+	defer res.Close()
+
 	for {
 		window.IMShow(img)
 		if window.WaitKey(1) >= 0 {
